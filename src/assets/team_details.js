79853568d3d880d2baa4d75b703287cdb1f1236a
Krishna Yadav <krishna.gusknp2023@ce.du.ac.in>
2024-09-15 19:57:02 +0530

    BroqWorks

---
+++ src/assets/team_details.js
@@ -0,0 +1,24 @@
+
+import Manager from './Manager.jpg'
+import Founder from './Founder.png'
+import AbcXyz from './AbcXyz.png'
+
+const Team_details = [
+  {
+    s_image: Founder,
+    s_name: "Nihal Thakur",
+    s_role: "Founder, BroqWorks"
+  },
+  {
+    s_image: Manager,
+    s_name: "Kamal Saloi",
+    s_role: "Manager, BroqWorks"
+  },
+  {
+    s_image: AbcXyz,
+    s_name: "Krishna Yadav",
+    s_role: "AbcXyz, BroqWorks"
+  }
+];
+
+export default Team_details;
