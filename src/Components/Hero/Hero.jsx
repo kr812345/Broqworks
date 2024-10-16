d245f2b84f90901f516283d4d0139c7aab0052bd
Krishna Yadav <krishna.gusknp2023@ce.du.ac.in>
2024-10-16 11:15:36 +0530

    New changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,8 @@ import profile_img from '../../assets/profile_img.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import logo from '../../assets/logo.webp'
 import home_img from '../../assets/home_img.svg'
-
+import call_icon from '../../assets/call_icon.svg'
+import mail_icon from '../../assets/mail_icon.svg'
 
 const Hero = () => {
   return (
@@ -27,9 +28,10 @@ const Hero = () => {
       But don’t worry, from strategic marketing to custom designs and photography, BroqWorks equips businesses with everything they need to attract more clients and thrive in a competitive market.
       </p>
       <div className='contact-details'>
-      <h5>+91-8178-584-661</h5>
-      <h5>getintouch@broqworks.com</h5>
+      <h5><a href="tel:8178584661"><img className='icon_' src={call_icon} alt="" /><p>+91-8178-584-661</p></a></h5>
+      <h5><a href="mailto:getintouch@broqworks.com"><img className='icon_' src={mail_icon} alt="" /><p>getintouch@broqworks.com</p></a></h5>
       </div>
+      <div className="connect"><AnchorLink className='anchor-link' offset={50} href='#contact'><h5>Connect With Us</h5></AnchorLink></div>
     </div>
 
     <div className='right-side'>
